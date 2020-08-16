ce2fb056eefb478d3c479c4fa651511385cba831
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 13:47:02 +0530

    Assignment 1 : Added Test for scenario 3

--- UnitTestAssignment/ProcessOrderTest.cs
+++ UnitTestAssignment/ProcessOrderTest.cs
@@ -7,8 +7,8 @@ namespace UnitTestAssignment
     [TestClass]
     public class ProcessOrderTest
     {
-        [TestMethod]
-        public void NoPromotpnOrderProcessdSuccessfully()
+        [TestMethod, TestCategory("smoke")]
+        public void NoPromotionOrderProcessdSuccessfully()
         {
             // Arrange
             var order = new Order();
@@ -27,8 +27,8 @@ namespace UnitTestAssignment
             Assert.AreEqual(result, 100);
         }
 
-        [TestMethod]
-        public void WithSingleProductPromotinOrderProcessdSuccessfully()
+        [TestMethod, TestCategory("smoke")]
+        public void WithSingleProductPromotionOrderProcessdSuccessfully()
         {
             // Arrange
             var order = new Order();
@@ -39,7 +39,7 @@ namespace UnitTestAssignment
             order.Cart.Items.Add(new CartItem { Product = new Product { Price = 20.0M, Sku = "C" }, Quantity = 1 });
 
             order.AddPromotion(new Promotion { Code = "A", DiscountPrice = 130, ProductSku = "A", Quantity = 3, Type = "BuyXForY" });
-            order.AddPromotion(new Promotion { Code = "B", DiscountPrice = 45, ProductSku = "B", Quantity = 3, Type = "BuyXForY" });
+            order.AddPromotion(new Promotion { Code = "B", DiscountPrice = 45, ProductSku = "B", Quantity = 2, Type = "BuyXForY" });
 
             // Act
             var engine = new CalculationEngine();
@@ -48,6 +48,32 @@ namespace UnitTestAssignment
 
             // Assert
             Assert.AreEqual(result, 370);
+        }
+
+        [TestMethod, TestCategory("smoke")]
+        public void WithCombinationProductPromotionOrderProcessdSuccessfully()
+        {
+            // Arrange
+            var order = new Order();
+
+            order.Cart = new Cart();
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 50.0M, Sku = "A" }, Quantity = 3 });
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 30.0M, Sku = "B" }, Quantity = 5 });
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 20.0M, Sku = "C" }, Quantity = 1 });
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 15.0M, Sku = "D" }, Quantity = 1 });
+
+            order.AddPromotion(new Promotion { Code = "A", DiscountPrice = 130, ProductSku = "A", Quantity = 3, Type = "BuyXForY" });
+            order.AddPromotion(new Promotion { Code = "B", DiscountPrice = 45, ProductSku = "B", Quantity = 2, Type = "BuyXForY" });
+            order.AddPromotion(new Promotion { Code = "CD", DiscountPrice = 30, ProductSku = "C", Quantity = 1, Type = "BuyXYForZ" });
+            order.AddPromotion(new Promotion { Code = "CD", DiscountPrice = 30, ProductSku = "C", Quantity = 1, Type = "BuyXYForZ" });
+
+            // Act
+            var engine = new CalculationEngine();
+
+            var result = engine.CalculateNetAmount(order);
+
+            // Assert
+            Assert.AreEqual(result, 280);
         }
 
     }
