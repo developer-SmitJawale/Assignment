4e0559542423a0cab1e34e3eb2f5aaa28d475327
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 12:30:15 +0530

    Assignment 1 : Act and assert the calculation engine

--- UnitTestAssignment/ProcessOrderTest.cs
+++ UnitTestAssignment/ProcessOrderTest.cs
@@ -16,6 +16,14 @@ namespace UnitTestAssignment
             order.Cart.Items.Add(new CartItem { Product = new Product { Price = 50.0M, Sku = "A" }, Quantity = 1 });
             order.Cart.Items.Add(new CartItem { Product = new Product { Price = 30.0M, Sku = "B" }, Quantity = 1 });
             order.Cart.Items.Add(new CartItem { Product = new Product { Price = 20.0M, Sku = "C" }, Quantity = 1 });
+
+            // Act
+            var engine = new CalculationEngine();
+
+            var result = engine.CalculateNetAmount(order);
+
+            // Assert
+            Assert.AreEqual(result, 100);
         }
     }
 }
