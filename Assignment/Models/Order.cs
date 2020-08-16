dd7bdd58c0751c068b056a500063bde04acda88d
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 12:07:36 +0530

    Assignment1 : Added Order

---
+++ Assignment/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Models
+{
+    public abstract class OrderBase
+    {
+
+    }
+
+    public class Order : OrderBase
+    {
+    }
+}
