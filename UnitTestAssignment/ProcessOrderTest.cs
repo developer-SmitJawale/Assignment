00319ebef90d9a3bd7f800e3c747929a2ea4e846
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 12:19:17 +0530

    Assignment1 : Add Product In cart Test

--- UnitTestAssignment/ProcessOrderTest.cs
+++ UnitTestAssignment/ProcessOrderTest.cs
@@ -13,7 +13,9 @@ namespace UnitTestAssignment
             var order = new Order();
 
             order.Cart = new Cart();
-
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 50.0M, Sku = "A" }, Quantity = 1 });
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 30.0M, Sku = "B" }, Quantity = 1 });
+            order.Cart.Items.Add(new CartItem { Product = new Product { Price = 20.0M, Sku = "C" }, Quantity = 1 });
         }
     }
 }
