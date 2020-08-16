850136bba72819d309d8c3e07034b265cb5a23ec
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 12:03:42 +0530

    Assignment1 : Added OrderProcess Test

---
+++ UnitTestAssignment/ProcessOrderTest.cs
@@ -0,0 +1,17 @@
+using Assignment.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestAssignment
+{
+    [TestClass]
+    public class ProcessOrderTest
+    {
+        [TestMethod]
+        public void NoPromotpnOrderProcessdSuccessfully()
+        {
+            // Arrange
+            var order = new Order();
+
+        }
+    }
+}
