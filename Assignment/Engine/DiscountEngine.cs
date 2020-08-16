8847c9c4af6e84c34597fce4ea3debb02925915f
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 13:10:54 +0530

    Assignment 1 : Added Discount Engine by implement strategy pattern

---
+++ Assignment/Engine/DiscountEngine.cs
@@ -0,0 +1,20 @@
+﻿using Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Engine
+{
+    /// <summary>
+    /// Discount Engine
+    /// Implemented using strategy pattern
+    /// </summary>
+    public abstract class DiscountEngine
+    {
+        public virtual Promotion MyProperty { get; set; }
+
+        public OrderBase OrderBase { get; set; }
+
+        public abstract decimal CalculateDiscount();
+    }
+}
