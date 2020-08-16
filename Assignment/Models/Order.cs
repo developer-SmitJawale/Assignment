647bccb6262156d8b7452a9b6bfd8faa6cff6ea2
Smit Jawale <developer.jawalesmit@gmail.com>
2020-08-16 13:03:23 +0530

    Assignment 1 : Added Promotion in Order with Aggregate root pattern

--- Assignment/Models/Order.cs
+++ Assignment/Models/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Assignment.Models
 {
@@ -13,6 +14,27 @@ namespace Assignment.Models
 
     public class Order : OrderBase
     {
+        public Order()
+        {
+            this.Promotions = new List<Promotion>();
+        }
+
         public decimal NetAmount { get; set; }
+
+        // 
+        /// <summary>
+        /// Design Pattern : Aggregate root pattern
+        /// For validating/restricting no more than one promotions are added for same product 
+        /// </summary>
+        private List<Promotion> Promotions { get; set; }
+
+        public void AddPromotion(Promotion promotion)
+        {
+            if(Promotions.FirstOrDefault(p=>p.ProductSku == promotion.ProductSku) == null)
+            {
+                this.Promotions.Add(promotion);
+            }
+        }
+
     }
 }
---
+++ Assignment/Models/Promotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Models
+{
+    public class Promotion
+    {
+        public string Code { get; set; }
+
+        public string ProductSku { get; set; }
+
+        public string Type { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal? DiscountPrice { get; set; }
+
+        public decimal? DiscountPercentage { get; set; }
+    }
+}
